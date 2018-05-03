35e57e43d28943a3bac473b243f1ef33cd8514e1
tomkim825 <tk825@hotmail.com>
2018-05-03 14:48:17 -0700

    corrected syntax for timing bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -52,6 +52,7 @@ $(document).ready(function() {
     var questionsLeft = quizInfo.length;
     var intervalId;
     var number;
+    var number1;
     var gameover= false;
 
 //      ****************************************
@@ -60,107 +61,119 @@ $(document).ready(function() {
 //      **    Gameplay part of script below   **
 //      ****************************************
 
-function stopTimer() {
-    clearInterval(intervalId);
-};
-
-function runTimer(number) {
-    intervalId = setInterval(function countDown() {
-        $('#time-left').text(number);
-        number --;
-        if (number === 0) {
-            showAnswer()
-            stopTimer();
-        };    
-    }, 1000);
-};
-
-// [4th event] clear timer, set it to 3 sec, start timer, stylize answers green/red and show
-// after this, move on to next question if there are questions left
-  function showAnswer() {
-        $('#time-left').text("");
-        $('.answer-box').css("background-image",correctPicture).css("background-size","contain").css("background-repeat","no-repeat").css("background-position","right 10%");
-        $('#score').text(score);
-        if(answeredCorrectly){ $('#question').text("CORRECT!");} else {$('#question').text("WRONG!");};
-        for(i=1;i<(quizInfo[0].answers.length+1);i++){
-        $('#answer-'+i).css("border-color","red").css("color","red").css("background-color","pink").css("opacity","0.2");
-        };
-        $('#'+correctAnswer).css("border-color","green").css("color","green").css("background-color","white").css("opacity","1");
-       
-        stopTimer();    
-        runTimer(2);
-        setTimeout(nextQuestion, 3500);
-         questionsLeft --;
-         // checks if last question answered
-        if(questionsLeft === 0){
-            setTimeout(function(){
-                gameover = true; 
-                for(i=1;i<(quizInfo[0].answers.length+1);i++){
-                    $('#answer-'+i).css("border-color","black").css("color","black").css("background-color","white").css("opacity","1").css("filter","blur(0px)");
-                    };
-                $('#question').text("Game Over");
-                $('#answer-1').text("Correct: "+ score);
-                $('#answer-2').text("Incorrect: "+ (quizInfo.length-score));
-                $('#answer-3').css("display","none");
-                $('#playagain').css("display","block").on('click',function(){resetGame();});
-            }, 3500);
-        };
-    };  
-
-// [2nd event]. Resets styling on choices and iterates (or intializes) to next question. Then written to HTML. For-loop resets css. question info grabbed from array iterated by x, which is advanced by x++ at end. If x reaches all questions, gameover() called 
-    function nextQuestion(){
-        $('#time-left').text("");
-        // (re)sets styling on all 3 answer choices
-        for(i=1;i<(quizInfo[i].answers.length+1);i++){
-            $('#answer-'+i).css("border-color","gray").css("color",'#555').css("background-color","rgb(230, 230, 230)").css("opacity","1");
-        };
-        // removes answer picture
-        $('.answer-box').css("background-image","")
-        // iterates through the info array/object and stores info in variables
-        quizQuestion = quizInfo[x].question;
-        quizAnswer = quizInfo[x].answers;
-        correctAnswer = quizInfo[x].correct;
-        correctPicture = quizInfo[x].picture;
-        // updates HTML with quiz info
-        $('#question').text(quizQuestion);
-        $('#answer-1').text(quizAnswer[0]);
-        $('#answer-2').text(quizAnswer[1]);
-        $('#answer-3').text(quizAnswer[2]);
-        // Iterates x (to go through quiz info)
-        x++;
-        stopTimer();
-        runTimer(7);
+    function stopTimer() {
+        clearInterval(intervalId);
     };
 
-// [1st event] Start game with enter key press. Set gamestarted = true to prevent strange behavior if enter key pressed accidentally during quiz. answer fields will become visible
-// (next) go to nextQuestion to begin 30 sec timer and advance to questions
-    $("body").keypress(function (event) {
-        if ((event.key == "Enter") && (!gameStarted)){
-            gameStarted = true
-            // sets answer field visible and removes "play again?" element 
-            $('.answer').css('display','block');
+    function runTimer(number) {
+        intervalId = setInterval(function countDown() {
+            $('#time-left').text(number);
+            number --;
+            if (number === 0) {
+                answeredCorrectly = false;
+                stopTimer();
+                showAnswer();                
+            };    
+        }, 1000);
+    };
+
+    function answerTimer(number1) {
+        intervalId = setInterval(function countDown() {
+            $('#time-left').text(number1);
+            number1 --;
+            if (number1 === 0) {
+                stopTimer();
+            };    
+        }, 1000);
+    };
+
+    // [4th event] clear timer, set it to 3 sec, start timer, stylize answers green/red and show
+    // after this, move on to next question if there are questions left
+    function showAnswer() {
+            $('#time-left').text("");
+            $('.answer-box').css("background-image",correctPicture).css("background-size","contain").css("background-repeat","no-repeat").css("background-position","right 10%");
+            $('#score').text(score);
+            if(answeredCorrectly){ $('#question').text("CORRECT!");} else {$('#question').text("WRONG!");};
+            for(i=1;i<(quizInfo[0].answers.length+1);i++){
+            $('#answer-'+i).css("border-color","red").css("color","red").css("background-color","pink").css("opacity","0.2");
+            };
+            $('#'+correctAnswer).css("border-color","green").css("color","green").css("background-color","white").css("opacity","1");
+        
+            stopTimer();
+            number1 = 2;    
+            answerTimer(2);
+            setTimeout(nextQuestion, 3500);
+            questionsLeft --;
+            // checks if last question answered
+            if(questionsLeft === 0){
+                setTimeout(function(){
+                    gameover = true; 
+                    for(i=1;i<(quizInfo[0].answers.length+1);i++){
+                        $('#answer-'+i).css("border-color","black").css("color","black").css("background-color","white").css("opacity","1").css("filter","blur(0px)");
+                        };
+                    $('#question').text("Game Over");
+                    $('#answer-1').text("Correct: "+ score);
+                    $('#answer-2').text("Incorrect: "+ (quizInfo.length-score));
+                    $('#answer-3').css("display","none");
+                    $('#playagain').css("display","block").on('click',function(){resetGame();});
+                }, 3500);
+            };
+        };  
+
+    // [2nd event]. Resets styling on choices and iterates (or intializes) to next question. Then written to HTML. For-loop resets css. question info grabbed from array iterated by x, which is advanced by x++ at end. If x reaches all questions, gameover() called 
+        function nextQuestion(){
+            $('#time-left').text("");
+            // (re)sets styling on all 3 answer choices
+            for(i=1;i<(quizInfo[i].answers.length+1);i++){
+                $('#answer-'+i).css("border-color","gray").css("color",'#555').css("background-color","rgb(230, 230, 230)").css("opacity","1");
+            };
+            // removes answer picture
+            $('.answer-box').css("background-image","")
+            // iterates through the info array/object and stores info in variables
+            quizQuestion = quizInfo[x].question;
+            quizAnswer = quizInfo[x].answers;
+            correctAnswer = quizInfo[x].correct;
+            correctPicture = quizInfo[x].picture;
+            // updates HTML with quiz info
+            $('#question').text(quizQuestion);
+            $('#answer-1').text(quizAnswer[0]);
+            $('#answer-2').text(quizAnswer[1]);
+            $('#answer-3').text(quizAnswer[2]);
+            // Iterates x (to go through quiz info)
+            x++;
+            stopTimer();
+            runTimer(7);
+        };
+
+    // [1st event] Start game with enter key press. Set gamestarted = true to prevent strange behavior if enter key pressed accidentally during quiz. answer fields will become visible
+    // (next) go to nextQuestion to begin 30 sec timer and advance to questions
+        $("body").keypress(function (event) {
+            if ((event.key == "Enter") && (!gameStarted)){
+                gameStarted = true
+                // sets answer field visible and removes "play again?" element 
+                $('.answer').css('display','block');
+                $('#playagain').css("display","none");
+                nextQuestion();
+            }
+        });
+
+
+    // *****************************************
+
+    //[last event] resets variables for next game and starts questions over again from 1st one.
+        function resetGame() {
+            gameover = false;
+            questionsLeft = quizInfo.length;
+            x=0;
+            score=0;
+            $('#score').text(score);
+            $('#answer-3').css("display","block");
             $('#playagain').css("display","none");
             nextQuestion();
-        }
-    });
+        };
 
-
-// *****************************************
-
-//[last event] resets variables for next game and starts questions over again from 1st one.
-    function resetGame() {
-        gameover = false;
-        questionsLeft = quizInfo.length;
-        x=0;
-        score=0;
-        $('#score').text(score);
-        $('#answer-3').css("display","block");
-        $('#playagain').css("display","none");
-        nextQuestion();
-    };
-
-// [3rd event] User clicks on an answer. Timer stops If not gameover, checks if correct answer, updates score. 
-// Sets answeredCorrectly to true or false to later show "Correct or Wrong". Then showAnswer function
+    // [3rd event] User clicks on an answer. Timer stops If not gameover, checks if correct answer, updates score. 
+    // Sets answeredCorrectly to true or false to later show "Correct or Wrong". Then showAnswer function
     
     $('.answer').on('click', function() {
         if(!gameover){
